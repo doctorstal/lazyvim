c7f29a33c57986e451b5f8235fb5f83327bd1f6d
Oleksii Stadnyk <doctorstal@gmail.com>
2025-04-24 14:17:08 +0200

    Update kage plugins

--- lua/plugins/kage.lua
+++ lua/plugins/kage.lua
@@ -1,5 +1,5 @@
 return {
-  "SirVer/ultisnips",
-  "jayli/vim-easycomplete",
-  "sedyh/ebitengine-kage-vim",
+  { ft = { "kage" }, "SirVer/ultisnips" },
+  { ft = { "kage" }, "jayli/vim-easycomplete" },
+  { ft = { "kage" }, "sedyh/ebitengine-kage-vim" },
 }
