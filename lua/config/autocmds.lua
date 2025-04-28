907dd214d5cb42101f1fc674d5c402b2d9b2fbda
Oleksii Stadnyk <oleksii.stadnyk@truecaller.com>
2025-04-28 22:10:26 +0200

    Disable autoformat for scala files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,9 +7,16 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-local mygroup = vim.api.nvim_create_augroup("wall_on_edit", { clear = true })
 vim.api.nvim_create_autocmd({ "FocusLost", "TextChanged" }, {
   pattern = "*",
-  group = mygroup,
+  group = vim.api.nvim_create_augroup("wall_on_edit", { clear = true }),
   command = "wall",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("filetype_scala", { clear = true }),
+  pattern = { "scala", "sbt" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
 })
